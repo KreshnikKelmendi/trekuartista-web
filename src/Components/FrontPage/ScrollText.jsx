7eddac1e0bf8ae3d3876c20c3cdcbf94b938b1af
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-10-12 23:41:18 +0200

    partnert

--- src/Components/FrontPage/ScrollText.jsx
+++ src/Components/FrontPage/ScrollText.jsx
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react';
 const ScrollText = () => {
     const { scrollYProgress } = useScroll();
     const x = useTransform(scrollYProgress, [1, 0], [-105, 750]);
-    // Array of services
+
     const services = [
         {
-            title: 'Brandind & Identity',
+            title: 'Branding & Identity',
             content: `At Trekuartista, we believe that every brand has a unique story to tell. We focus on crafting a visual identity that embodies your core values and resonates with your audience. From logo design to brand guidelines, the aim is to establish a distinctive presence that authentically represents you. Let’s work together to create a brand that makes a lasting impact!`
         },
         {
@@ -28,11 +28,9 @@ const ScrollText = () => {
         }
     ];
 
-    // State to keep track of current service index
     const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
     const [blurContent, setBlurContent] = useState(true);
 
-    // Function to handle next service
     const nextService = () => {
         setCurrentServiceIndex((prevIndex) => (prevIndex === services.length - 1 ? 0 : prevIndex + 1));
         setBlurContent(true);
@@ -41,7 +39,6 @@ const ScrollText = () => {
         }, 1000);
     };
 
-    // Function to handle previous service
     const prevService = () => {
         setCurrentServiceIndex((prevIndex) => (prevIndex === 0 ? services.length - 1 : prevIndex - 1));
         setBlurContent(true);
@@ -50,7 +47,6 @@ const ScrollText = () => {
         }, 1000);
     };
 
-    // Automatically change services every 5 seconds
     useEffect(() => {
         const intervalId = setInterval(() => {
             nextService();
@@ -62,22 +58,40 @@ const ScrollText = () => {
     return (
         <>
             <div className='bg-black lg:py-[102px] overflow-hidden textTransition justify-center items-center mx-auto text-center'>
-                <motion.h1 className='hidden lg:flex text-white font-custom text-4xl lg:text-[60px] 2xl:text-[95px]' style={{ x }}>We craft digital experiences</motion.h1>
+                <motion.h1 
+                    className='hidden lg:flex text-white font-custom text-4xl lg:text-[60px] 2xl:text-[95px]'
+                    style={{ x }}
+                    transition={{ duration: 1, ease: [0.25, 0.1, 0.25, 1] }}
+                >
+                    We craft digital experiences
+                </motion.h1>
             </div>
             <div className='bg-black pt-16 lg:py-[102px] lg:hidden justify-center items-center mx-auto px-4'>
-                <motion.h1 className=' text-white font-custom text-4xl lg:text-[60px] 2xl:text-[95px]'>We craft digital experiences</motion.h1>
+                <motion.h1 className='text-white font-custom text-4xl lg:text-[60px] 2xl:text-[95px]'>
+                    We craft digital experiences
+                </motion.h1>
             </div>
 
-
-            {/* Display current service with smooth transitions */}
             <div className='text-white bg-black lg:h-[60vh] px-5 lg:px-[60px] py-16 lg:py-[100px] flex flex-col lg:flex-row lg:justify-between'>
                 <div className='lg:w-1/2'>
-                    <p className='font-custom text-4xl lg:text-[45px] leading-tight w-fit lg:w-[520px]'>{services[currentServiceIndex].title}</p>
+                    <motion.p 
+                        className='font-custom text-4xl lg:text-[45px] leading-tight w-fit lg:w-[520px]'
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        transition={{ duration: 1, ease: [0.25, 0.1, 0.25, 1] }}
+                    >
+                        {services[currentServiceIndex].title}
+                    </motion.p>
                 </div>
                 <div className='lg:w-1/2 py-5'>
-                    <p className={`font-custom1 lg:w-[520px] 2xl:w-[665px] lg:h-[342px] text-[16px] transition-all duration-1000 ${blurContent ? '' : ''}`}>
+                    <motion.p 
+                        className={`font-custom1 lg:w-[520px] 2xl:w-[665px] lg:h-[342px] text-[16px] transition-all duration-1000`}
+                        initial={{ opacity: 0, scale: 0.95 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 1, ease: [0.25, 0.1, 0.25, 1] }}
+                    >
                         {services[currentServiceIndex].content}
-                    </p>
+                    </motion.p>
                 </div>
                 <div className='lg:flex flex justify-center lg:justify-normal flex-row lg:flex-col gap-y-2 gap-x-2 lg:gap-x-0'>
                     {services.map((service, index) => (
