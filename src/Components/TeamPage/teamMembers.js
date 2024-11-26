f26d214089861be25f13c98f5d0d235648044fc5
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2024-11-26 11:32:06 +0100

    roni

--- src/Components/TeamPage/teamMembers.js
+++ src/Components/TeamPage/teamMembers.js
@@ -27,155 +27,151 @@ import edmiri from "../Assets/DSC_5170.jpg"
 import mirjeta1 from "../Assets/mirjeta.jpg"
 import mirjeta from "../Assets/mirjeta1.jpg"
 import member45 from "../Assets/EuronNika.png"
+import euron1 from "../Assets/euron.png"
+import euron2 from "../Assets/euron2.png"
+import liridona1 from "../Assets/liridona1.png"
+import liridona2 from "../Assets/liridona2.png"
 
 const teamMembers = [
-        
-        // Member 1
-    {
-      id: 1,
-      name: 'Arian Ahmeti',
-      position: 'CEO Founder',
-      image: ariani,
-      hoverImage: member2,
-      linkedinLink: ""
-    },
-        // Member 2
-    {
-      id: 2,
-      name: 'Alba Krasniqi',
-      position: 'Operations Manager',
-      image: member3,
-      hoverImage: alba,
-      linkedinLink: "https://www.linkedin.com/in/alba-krasniqi-87b93a105/"
-    },
-        // Member 3
-        {
-          id: 3,
-          name: 'Edmir Kuleta',
-          position: 'Team Leader',
-          image: member9,
-          hoverImage: edmiri,
-        },
-        // Member 4
-    {
-      id: 4,
-      name: 'Kestrina Hajrizi',
-      position: 'Finance Manager',
-      image: kestrina,
-      hoverImage: member4,
-      linkedinLink: 'https://www.linkedin.com/in/kestrina-hajrizi-92683025a/'
-    },
-    {
-      id: 7,
-      name: 'Gentrit Mehmeti',
-      position: 'Senior Graphic & UI/UX Designer',
-      image: member8,
-      hoverImage: gentrit,
-      linkedinLink: 'https://www.linkedin.com/in/gentrit-mehmeti-5a88511b3/'
-    },
-    
-        // Member 5
-    {
-      id: 5,
-      name: 'Dora Kryeziu',
-      position: 'Account Manager',
-      image: member5,
-      hoverImage: dora,
-      linkedinLink: 'https://www.linkedin.com/in/dora-kryeziu-872299255/'
-    },
-      // Member 6
-      {
-        id: 11,
-        name: 'Endrit Morina',
-        position: '3D Artist',
-        image: member14,
-        hoverImage: endrit, 
-        linkedinLink:'https://www.linkedin.com/in/endritmorina/'
-      },
-      {
-        id: 9,
-        name: 'Hana Jaha',
-        position: 'Graphic Designer',
-        image: member11,
-        hoverImage: hana,
-        linkedinLink: 'https://www.linkedin.com/in/hana-jaha/'
-      },
-      {
-        id: 8,
-        name: 'Eduart Demaj',
-        position: 'Graphic Designer',
-        image: member10,
-        hoverImage: eduart,
-        linkedinLink: 'https://www.linkedin.com/in/eduart-demaj-524641236/'
-      },
-      {
-        id: 10,
-        name: 'Andena Rexhepi',
-        position: 'Graphic Designer',
-        image: member12,
-        hoverImage: andena, 
-        linkedinLink: 'https://www.linkedin.com/in/andena-rexhepi-069108258/'
-      },
-      
-      {
-        id: 14,
-        name: 'Kreshnik Kelmendi',
-        position: 'Web Developer',
-        image: member15,
-        hoverImage: kreshnik, 
-        linkedinLink: 'https://www.linkedin.com/in/kreshnik-kelmendi-90b796242/'
-      },
-      {
-        id: 13,
-        name: 'Fiona Rrecaj',
-        position: 'TikTok Creator',
-        image: member16,
-        hoverImage: fiona, 
-        linkedinLink: 'https://www.linkedin.com/in/fiona-rrecaj-45a0b5236/'
-      },
-      {
-        id: 12,
-        name: 'Fuad Shatri',
-        position: 'Motion Designer',
-        image: member13,
-        hoverImage: fuad, 
-        linkedinLink: 'https://www.linkedin.com/in/fuad-shatri-7411371a5/'
-      },
-    {
-      id: 6,
-      name: 'Mirjeta Murselaj',
-      position: 'Finance Officer',
-      image: mirjeta1,
-      hoverImage: mirjeta,
-      linkedinLink: 'https://www.linkedin.com/in/mirjeta-murselaj-2a0a94162/'
-    },
-    // {
-    //     id: 10,
-    //     name: 'Euron Nika',
-    //     position: 'Graphic Designer',
-    //     image: member45,
-    //     // hoverImage: euroni, 
-    //     linkedinLink: 'https://www.linkedin.com/in/andena-rexhepi-069108258/'
-    //   },
-        // Member 7
-    
-        // Member 8
-   
-        // Member 9
-    
-        // Member 10
-   
-    // Member 11
-   
-    // Member 12
-  
-    // Member 13
-    
-    // Member 14
-   
+
+  // Member 1
+  {
+    id: 1,
+    name: 'Arian Ahmeti',
+    position: 'CEO Founder',
+    image: ariani,
+    hoverImage: member2,
+    linkedinLink: ""
+  },
+  // Member 2
+  {
+    id: 2,
+    name: 'Alba Krasniqi',
+    position: 'Operations Manager',
+    image: member3,
+    hoverImage: alba,
+    linkedinLink: "https://www.linkedin.com/in/alba-krasniqi-87b93a105/"
+  },
+  // Member 3
+  {
+    id: 3,
+    name: 'Edmir Kuleta',
+    position: 'Team Leader',
+    image: member9,
+    hoverImage: edmiri,
+  },
+  // Member 4
+  {
+    id: 4,
+    name: 'Kestrina Hajrizi',
+    position: 'Finance Manager',
+    image: kestrina,
+    hoverImage: member4,
+    linkedinLink: 'https://www.linkedin.com/in/kestrina-hajrizi-92683025a/'
+  },
+  {
+    id: 7,
+    name: 'Gentrit Mehmeti',
+    position: 'Senior Graphic & UI/UX Designer',
+    image: member8,
+    hoverImage: gentrit,
+    linkedinLink: 'https://www.linkedin.com/in/gentrit-mehmeti-5a88511b3/'
+  },
+
+  // Member 5
+  {
+    id: 5,
+    name: 'Dora Kryeziu',
+    position: 'Account Manager',
+    image: member5,
+    hoverImage: dora,
+    linkedinLink: 'https://www.linkedin.com/in/dora-kryeziu-872299255/'
+  },
+  // Member 6
+  {
+    id: 11,
+    name: 'Endrit Morina',
+    position: '3D Artist',
+    image: member14,
+    hoverImage: endrit,
+    linkedinLink: 'https://www.linkedin.com/in/endritmorina/'
+  },
+  {
+    id: 9,
+    name: 'Hana Jaha',
+    position: 'Graphic Designer',
+    image: member11,
+    hoverImage: hana,
+    linkedinLink: 'https://www.linkedin.com/in/hana-jaha/'
+  },
+  {
+    id: 8,
+    name: 'Eduart Demaj',
+    position: 'Graphic Designer',
+    image: member10,
+    hoverImage: eduart,
+    linkedinLink: 'https://www.linkedin.com/in/eduart-demaj-524641236/'
+  },
+  {
+    id: 10,
+    name: 'Andena Rexhepi',
+    position: 'Graphic Designer',
+    image: member12,
+    hoverImage: andena,
+    linkedinLink: 'https://www.linkedin.com/in/andena-rexhepi-069108258/'
+  },
+
+  {
+    id: 14,
+    name: 'Kreshnik Kelmendi',
+    position: 'Web Developer',
+    image: member15,
+    hoverImage: kreshnik,
+    linkedinLink: 'https://www.linkedin.com/in/kreshnik-kelmendi-90b796242/'
+  },
+  {
+    id: 13,
+    name: 'Fiona Rrecaj',
+    position: 'TikTok Creator',
+    image: member16,
+    hoverImage: fiona,
+    linkedinLink: 'https://www.linkedin.com/in/fiona-rrecaj-45a0b5236/'
+  },
+  {
+    id: 12,
+    name: 'Fuad Shatri',
+    position: 'Motion Designer',
+    image: member13,
+    hoverImage: fuad,
+    linkedinLink: 'https://www.linkedin.com/in/fuad-shatri-7411371a5/'
+  },
+  {
+    id: 6,
+    name: 'Mirjeta Murselaj',
+    position: 'Finance Officer',
+    image: mirjeta1,
+    hoverImage: mirjeta,
+    linkedinLink: 'https://www.linkedin.com/in/mirjeta-murselaj-2a0a94162/'
+  },
+  {
+    id: 18,
+    name: 'Euron Nika',
+    position: 'Graphic & Motion Designer',
+    image: euron2,
+    hoverImage: euron1,
+    linkedinLink: 'https://www.linkedin.com/in/euron-nika-4a2211244/'
+  },
+  {
+    id: 19,
+    name: 'Liridona Avdullahi',
+    position: 'Social Media Manager',
+    image: liridona1,
+    hoverImage: liridona2,
+    linkedinLink: 'https://www.linkedin.com/in/euron-nika-4a2211244/'
+  },
 
 
-  ];
+];
 
 
-  export { teamMembers }
+export { teamMembers }
