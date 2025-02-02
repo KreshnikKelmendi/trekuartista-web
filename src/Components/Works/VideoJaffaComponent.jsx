49f92eaa6cf8840e54738aa191cf7bad7b973476
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2025-02-02 02:24:36 +0100

    freelast

--- src/Components/Works/VideoJaffaComponent.jsx
+++ src/Components/Works/VideoJaffaComponent.jsx
@@ -4,7 +4,7 @@ import { FaVolumeMute, FaVolumeUp, FaTimes } from "react-icons/fa";
 const VideoJaffaComponent = ({ src }) => {
   const videoRef = useRef(null);
   const [isMuted, setIsMuted] = useState(true);
-  const [isFullScreen, setIsFullScreen] = useState(false); // state for full-screen modal
+  const [isFullScreen, setIsFullScreen] = useState(false);
 
   const toggleMute = () => {
     setIsMuted((prevMuted) => {
@@ -20,52 +20,55 @@ const VideoJaffaComponent = ({ src }) => {
     setIsFullScreen(!isFullScreen);
   };
 
-  // Handle escape key press to close the full-screen modal
   useEffect(() => {
     const handleEscKey = (event) => {
       if (event.key === "Escape") {
-        setIsFullScreen(false); // Close modal if escape is pressed
+        setIsFullScreen(false);
       }
     };
 
-    // Add event listener for escape key
     if (isFullScreen) {
       window.addEventListener("keydown", handleEscKey);
     }
 
-    // Clean up the event listener when the component unmounts or modal is closed
     return () => {
       window.removeEventListener("keydown", handleEscKey);
     };
-  }, [isFullScreen]); // Run effect when the modal state changes
+  }, [isFullScreen]);
 
   return (
     <div className="relative">
       {/* Normal Video View */}
       <div className={`relative w-full h-full ${isFullScreen ? "hidden" : ""}`}>
-        <video
-          ref={videoRef}
-          src={src}
-          playsInline
-          autoPlay
-          loop
-          muted={isMuted}
-          className="w-full h-[22vh] object-cover lg:h-[90vh]"
-          onClick={toggleFullScreen}
-        />
+        <div className="relative group"> 
+          {/* Video */}
+          <video
+            ref={videoRef}
+            src={src}
+            playsInline
+            autoPlay
+            loop
+            muted={isMuted}
+            className="w-full h-[22vh] object-cover lg:h-[90vh]"
+            onClick={toggleFullScreen}
+          />
 
-        {/* Mute Button */}
-        <button
-          onClick={toggleMute}
-          className="absolute bottom-1 right-[1px] bg-white p-1 rounded-full"
-        >
-          {isMuted ? <FaVolumeMute size={8} /> : <FaVolumeUp size={8} />}
-        </button>
+          {/* Hover Overlay */}
+          <div className="absolute inset-0 bg-black/70 opacity-0 cursor-pointer group-hover:opacity-100 transition-opacity duration-300"></div>
+
+          {/* Mute Button */}
+          <button
+            onClick={toggleMute}
+            className="absolute bottom-1 right-[1px] bg-white p-1 rounded-full"
+          >
+            {isMuted ? <FaVolumeMute size={9} /> : <FaVolumeUp size={9} />}
+          </button>
+        </div>
       </div>
 
       {/* Fullscreen Modal View */}
       {isFullScreen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex justify-center items-center z-50">
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 cursor-pointer flex justify-center items-center z-50">
           <div className="relative">
             <video
               ref={videoRef}
